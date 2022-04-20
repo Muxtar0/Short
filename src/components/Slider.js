3f598c4534a186609aa2829b4a9bad61fdabbb97
Muxtar0 <84911084+Muxtar0@users.noreply.github.com>
2022-04-20 12:00:58 +0400

    initial commit

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -13,8 +13,8 @@ import {
   useRecoilState,
   useRecoilValue,
 } from 'recoil';
-import {datas} from '../datas/datas'
-import videos from '../datas/videos'
+// import {datas} from '../datas/datas'
+// import videos from '../datas/videos'
 import ThumbUpRoundedIcon from '@mui/icons-material/ThumbUpRounded';
 import ThumbDownRoundedIcon from '@mui/icons-material/ThumbDownRounded';
 import ReplyRoundedIcon from '@mui/icons-material/ReplyRounded';
@@ -25,6 +25,7 @@ import PauseRoundedIcon from '@mui/icons-material/PauseRounded';
 import VolumeOffRoundedIcon from '@mui/icons-material/VolumeOffRounded';
 import VolumeUpRoundedIcon from '@mui/icons-material/VolumeUpRounded';
 import {videoPlayState} from '../atoms/videoAtom'
+import { stepConnectorClasses } from '@mui/material';
 function Slider() {
   // window.addEventListener('load',() => {
   //   console.log()
@@ -32,10 +33,28 @@ function Slider() {
   const [isMuteVideo,setIsMuteVideo] = useState(false)
   const [isStopVideo,setIsStopVideo] = useState(false)
   const [likes,setLikes] = useState(0)
+  const [datas,setDatas] = useState([])
+  const [fins,setFins] = useState(false)
   const WindowHeight = document.documentElement.clientHeight;
   const videoRef = useRef()
   
-  
+  useEffect(() => {
+    fetch("https://dummyjson.com/products")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setDatas(result.products)
+          setFins(true)
+        },
+        // Not: Burada hataları yakalamak önemlidir.
+        // Bileşenimizde bug bulunmaması için, 'catch ()' bloğu yerine bulunan
+        // bu blok içinde hatalar yakalanır.
+        (error) => {
+          setFins(false)
+        }
+      )
+      
+  },[])
   
   
   
@@ -52,44 +71,47 @@ function Slider() {
         // console.log(document.querySelector("video"))
       }}
       onSwiper={(swiper) => {
-        document.querySelector("video").play()
+        // document.querySelector("video").play()
       }}
       className={`h-full w-full`}
     >
-      {datas.map((data,index) => (
-        <SwiperSlide key={data.key} className="h-screnn flex justify-center items-center">
-        <div className={`w-full sm:w-[400px] mx-auto my-auto h-full  sm:h-[80%]  flex items-center justify-center`}>
-          <div className="flex h-full   w-full sm:w-[90%] relative items-end justify-center">
-            <div className="h-full  w-full  relative">
-              <PauseMuteBTNS/>
-              <video ref={videoRef}  className="h-full object-cover sm:rounded-md w-full" loop autoPlay={true} muted >
-                  <source src={videos[index]} type="video/mp4"></source>
-              </video>
-              <div className="absolute left-0 bottom-0 w-full px-2 py-2">
-                <div>
-                  <p className="text-white mb-2" >{data.description}</p>
-                </div>
-                <div className="flex items-center justify-start">
-                  <img className="w-[40px] h-[40px] rounded-full" src="https://www.business2community.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640.png"  />
-                  <p className="text-white ml-2">{data.username}</p>
+      {fins && (
+        datas.map((data) =>(
+          <SwiperSlide key={data.id} className="h-screnn flex justify-center items-center">
+          <div className={`w-full sm:w-[400px] mx-auto my-auto h-full  sm:h-[80%]  flex items-center justify-center`}>
+            <div className="flex h-full   w-full sm:w-[90%] relative items-end justify-center">
+              <div className="h-full  w-full  relative">
+                <PauseMuteBTNS/>
+                {/* <video ref={videoRef}  className="h-full object-cover sm:rounded-md w-full" loop autoPlay={true} muted >
+                    <source src={videos[index]} type="video/mp4"></source>
+                </video> */}
+                <img className="h-full object-cover sm:rounded-md w-full" src={data.images[0]} />
+                <div className="absolute left-0 bottom-0 w-full px-2 py-2">
+                  <div>
+                    <p className="text-white mb-2" >{data.title}</p>
+                  </div>
+                  <div className="flex items-center justify-start">
+                    <img className="w-[40px] h-[40px] rounded-full" src="https://www.business2community.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640.png"  />
+                    <p className="text-white ml-2">{data.brand}</p>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className="flex sm:relative absolute right-0 bottom-16 sm:bottom-0 items-center px-2 sm:px-4 justify-center flex-col">
-              <button className="text-white flex items-center justify-center flex-col">
-                <ThumbUpRoundedIcon className="text-white"/> {likes}
-              </button>
-              <button className="text-white my-6 flex items-center justify-center flex-col">
-                <ThumbDownRoundedIcon className="text-white"/> Dislike
-              </button>
-              <button className="text-white flex items-center justify-center flex-col">
-                <ReplyRoundedIcon className="text-white"/> Share
-              </button>
+              <div className="flex sm:relative absolute right-0 bottom-16 sm:bottom-0 items-center px-2 sm:px-4 justify-center flex-col">
+                <button className="text-white flex items-center justify-center flex-col">
+                  <ThumbUpRoundedIcon className="text-white"/> {likes}
+                </button>
+                <button className="text-white my-6 flex items-center justify-center flex-col">
+                  <ThumbDownRoundedIcon className="text-white"/> Dislike
+                </button>
+                <button className="text-white flex items-center justify-center flex-col">
+                  <ReplyRoundedIcon className="text-white"/> Share
+                </button>
+              </div>
             </div>
           </div>
-        </div>
-      </SwiperSlide>
-      ))}
+        </SwiperSlide>
+        ))  
+      )}
     </Swiper>
       </div>
     </RecoilRoot>
